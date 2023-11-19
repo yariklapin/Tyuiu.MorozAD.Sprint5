83b11d0c8dc548a0d639edb8c64ab4275f55f050
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-11-19 16:55:42 +0500

    Task1

---
+++ Tyuiu.MorozAD.Sprint5.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MorozAD.Sprint5.Task1.V24.Lib
+{
+    public class DataService : ISprint5Task1V24
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = ((3*Math.Cos(x))/(4*x-0.5)) + Math.Sin(x) - 5*x -2;
+                strY = Convert.ToString(y);
+                if (x != stopValue && (4 * x - 0.5) != 0)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else if (x != stopValue && (4 * x - 0.5) == 0)
+                {
+                    y = 0;
+                    strY = Convert.ToString(y);
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MorozAD.Sprint5.Task1.V24/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MorozAD.Sprint5.Task1.V24.Lib;
+
+namespace Tyuiu.MorozAD.Sprint5.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Мороз А.Д. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый фай                         *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Мороз Артём Дмитриевич     | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция, (произвести табулирование) f(x) на заданном диапазоне     *");
+            Console.WriteLine("* [-5;5] с шагом 1. Произвести проверку деления на 0. При делении на 0    *");
+            Console.WriteLine("* вернуть значение 0.                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
